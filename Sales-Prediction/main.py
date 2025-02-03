f153d40583ea74530c0198c74c6ed9c855d2b881
Kungfu-Cuttlefish <syrusfreeman@westernwyoming.edu>
2025-02-03 05:52:53 +0000

    Ready for grading.

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Syrus Freeman
+# 2/2/25
 # Sales Prediction Programming Project
 # COSC 1010
 #
@@ -12,3 +12,9 @@
 # Calculate the projected profit.
 
 # Print the projected profit.
+
+total_sales = float(input('Enter the Projected Sales: '))
+
+profit = total_sales * 0.23
+
+print('The profit is $', (profit, ',.2f'))
