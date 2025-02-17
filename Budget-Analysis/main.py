4edd5d9c62ca13d058eb55b3e22da2ab3daa7797
Kungfu-Cuttlefish <syrusfreeman@westernwyoming.edu>
2025-02-17 02:54:35 +0000

    test

--- Budget-Analysis/main.py
+++ Budget-Analysis/main.py
@@ -8,6 +8,8 @@
 # Initialize variables 
 total = 0
 amount_over = 0
+amount_left = 0
+
 # Enter Budget
 print ('Enter the budget for the month')
 budget = int(input())
@@ -22,3 +24,6 @@ for day in range (1, 31) :
 if total > budget :
     total - budget = amount_over
     print ('You are over budget by', amount_over,)
+if total < budget :
+    budget - total = amount_left
+    print ('You are under budget by', amount_left)
