e40da715d2da2ba26b92f522c307c7abcb9eb874
Kungfu-Cuttlefish <syrusfreeman@westernwyoming.edu>
2025-02-23 19:20:02 +0000

    Ready for grading

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -1,7 +1,18 @@
 #
-# Name
-# Date
+# Syrus Freeman
+# 2/23/2025
 # Feet to Inches Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program.
+INCHES_PER_FOOT = 12
+def main () :
+
+    feet = int (input ('Enter a number of feet: '))
+
+    print (feet, 'equals', feet_to_inches(feet), 'inches.')
+
+def feet_to_inches (feet) : 
+    return feet * INCHES_PER_FOOT
+
+main ()
