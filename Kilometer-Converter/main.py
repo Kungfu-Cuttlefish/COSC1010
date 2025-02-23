9aec17105cb1801199848b9fcb840610290b21e7
Kungfu-Cuttlefish <syrusfreeman@westernwyoming.edu>
2025-02-23 18:54:36 +0000

    Testing

--- Kilometer-Converter/main.py
+++ Kilometer-Converter/main.py
@@ -1,7 +1,21 @@
 #
-# Name
-# Date
+# Syrus Freeman
+# 2/23/2025
 # Kilometer Converter Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+CONVERSION_FACTOR = 0.6214
+
+def main () :
+    # Get the distance in km
+    kilometers = float (input ('Enter a distance in kilometers: '))
+
+    # Display the distance converted into miles.
+    show_miles (kilometers)
+
+def show_miles (km) :
+    # Calculate miles
+    miles = km * CONVERSION_FACTOR
+
+    print (km, 'kilometers equals', miles, 'miles.')
