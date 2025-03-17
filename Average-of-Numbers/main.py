decaff85491d4bfc191e90e93efd6376aa5eaafa
Kungfu-Cuttlefish <syrusfreeman@westernwyoming.edu>
2025-03-17 02:21:23 +0000

    Ready for Grading

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,10 @@
 #
-# Name
-# Date
+# Syrus Freeman
+# 3/16/2025
 # Average of Numbers Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# Open the file
+myfile = open ('Average-of-Numbers//numbers.txt', 'r')
--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,18 @@
 #
-# Name
-# Date
+# Syrus Freeman
+# 3/16/2025
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# Open the file.
+myfile = open ('File-Display//numbers.txt', 'r')
+
+# Read and display file's contents.
+for line in myfile:
+    number = int (line)
+    print (number)
+
+# Close the file.
+myfile.close ()
