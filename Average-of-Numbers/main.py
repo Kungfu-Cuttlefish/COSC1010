48a99b84b5d929b9aa1e052867ea66fb3319ae29
Kungfu-Cuttlefish <syrusfreeman@westernwyoming.edu>
2025-03-17 02:35:47 +0000

    Ready for Grading

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -6,5 +6,15 @@
 #
 # Use comments liberally throughout the program. 
 
+total = 0
 # Open the file
 myfile = open ('Average-of-Numbers//numbers.txt', 'r')
+
+# Read and average the file's contents
+number_list = myfile.readlines()
+
+for number in number_list:
+    total = total + float (number)
+
+myfile.close ()
+print (total / len(number_list))
